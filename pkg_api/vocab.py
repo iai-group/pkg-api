49f41e3d25a7a5a320014229c1b5d2aa2c1bcb2a
Nolwenn <28621493+NoB0@users.noreply.github.com>
2023-05-22 08:47:18 +0200

    Create vocabulary class Fixes #13

---
+++ pkg_api/vocab.py
@@ -0,0 +1,17 @@
+"""Static class that maps labels to predicates in the PKG namespace.
+
+These predicates are used to create SPARQL queries.
+See example for a query to retrieve a preference for a given entity:
+    SELECT ?preference
+    {
+    ?who Vocab.PREFERENCE
+    [ Vocab.ENTITY ?entity ;
+      Vocab.PREFERENCE_WEIGHT ?preference ]
+    }
+"""
+
+
+class Vocab:
+    PREFERENCE = "pkg:preference"
+    PREFERENCE_WEIGHT = "pkg:weight"
+    ENTITY = "pkg:entity"
