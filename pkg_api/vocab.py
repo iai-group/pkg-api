41310c81ffb0a1d9e8c78c684f0ff4940da44982
Nolwenn <28621493+NoB0@users.noreply.github.com>
2023-05-26 17:36:32 +0200

    Docformatter

--- pkg_api/vocab.py
+++ pkg_api/vocab.py
@@ -1,6 +1,7 @@
 """Static class that maps labels to predicates in the PKG namespace.
 
 These predicates are used to create SPARQL queries.
+
 See example for a query to retrieve a preference for a given entity:
     SELECT ?preference
     {
