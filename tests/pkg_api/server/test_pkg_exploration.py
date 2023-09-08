386dc09f626bf3b48fc6715814516fa6d9270307
IKostric <ivica_kostric@yahoo.com>
2023-09-08 11:53:08 +0200

    Remove unused functions

--- tests/pkg_api/server/test_pkg_exploration.py
+++ tests/pkg_api/server/test_pkg_exploration.py
@@ -1,19 +1,3 @@
-import pytest
-from flask import Flask
-from flask_restful import Api
-
-from pkg_api.server.pkg_exploration import PKGExplorationResource
-
-
-@pytest.fixture
-def client():
-    app = Flask(__name__)
-    api = Api(app)
-    api.add_resource(PKGExplorationResource, "/explore")
-    client = app.test_client()
-    yield client
-
-
 def test_auth_endpoint(client):
     response = client.get("/explore")
     assert response.status_code == 200
