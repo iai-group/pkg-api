59a9882fcd28b4b0d8e2b8d4ad67d498d51a106c
Nolwenn <28621493+NoB0@users.noreply.github.com>
2023-11-21 17:05:44 +0100

    Remove changes to frontend related files

--- pkg_client/src/App.tsx
+++ pkg_client/src/App.tsx
@@ -1,23 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
 import APIHandler from "./APIHandler";
-import LoginForm from "./components/LoginForm/LoginForm";
-import Container from 'react-bootstrap/Container'
-import { UserContext } from "./contexts/UserContext";
 
 function App() {
-  const { user } = useContext(UserContext);
-
-  const content = !user ? <LoginForm /> : <APIHandler />;
-
   return (
-    <Container className="p-3">
-      <Container className="p-3 mb-4 bg-light rounded-3">
-        <div className="App">
-          {content}
-        </div>
-      </Container>
-    </Container>
+    <div className="App">
+      <APIHandler />
+    </div>
   );
 }
 
