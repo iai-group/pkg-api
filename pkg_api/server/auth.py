2f9c4f2f4e054f904964edee2ed82adf2ba53df2
Nolwenn <28621493+NoB0@users.noreply.github.com>
2024-01-02 14:32:35 +0100

    Address review comments

--- pkg_api/server/auth.py
+++ pkg_api/server/auth.py
@@ -6,6 +6,41 @@ from flask_restful import Resource
 
 
 class AuthResource(Resource):
-    def get(self) -> Dict[str, str]:
-        """Returns the authentication data."""
-        return {"message": "Prompt for login/signup"}
+    def post(self) -> Tuple[Dict[str, Any], int]:
+        """Logs in or registers the user.
+
+        Returns:
+            A dictionary with the user data and a message and the status code.
+        """
+        authentication_data = request.json
+        username = authentication_data.get("username", None)
+        password = authentication_data.get("password", None)
+        is_registration = authentication_data.get("isRegistration", False)
+
+        if not username or not password:
+            return {"message": "Missing username or password"}, 400
+
+        user = User.query.filter_by(username=username).first()
+
+        if is_registration:
+            if not user:
+                user = User(
+                    username=username,
+                    uri=create_user_uri(username),
+                    password=generate_password_hash(password),
+                )
+                db.session.add(user)
+                db.session.commit()
+            else:
+                return {"message": "This username already exists."}, 400
+        else:
+            if not user or not check_password_hash(user.password, password):
+                return {"message": "Invalid username or password."}, 401
+
+        return {
+            "user": {
+                "username": user.username,
+                "uri": user.uri,
+            },
+            "message": "Login successful",
+        }, 200
--- pkg_client/src/index.tsx
+++ pkg_client/src/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { UserProvider } from './contexts/UserContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
