cdec1bc538906af85b0de235077e84a28fc4d07a
Vinay Setty <1608263+vinaysetty@users.noreply.github.com>
2024-01-26 15:46:02 +0100

    Adds config file for ollama

--- pkg_api/nl_to_pkg/llm/llm_connector.py
+++ pkg_api/nl_to_pkg/llm/llm_connector.py
@@ -1,20 +1,27 @@
 """Module for querying LLM."""
+import yaml
 
 from typing import Any, Dict
 
 from ollama import Client, Options
+import os
 
 _DEFAULT_ENDPOINT = "http://badne4.ux.uis.no:11434"
 
 
 class LLMConnector:
-    def __init__(self, model: str = "llama2") -> None:
+    def __init__(
+        self, config_path: str = "pkg_api/nl_to_pkg/llm/llm_config_llama2.yaml"
+    ) -> None:
         """Initializes the LLMConnector class.
 
         Args:
-            model: The LLM to use, defaults to "llama2".
+            config_path: Path to the config file.
         """
-        self._model = model
+        self._config = self._load_config(config_path)
+        self._model = self._config.get("model", "llama2")
+        self._stream = self._config.get("stream", True)
+        self._llm_options = self._get_llm_config()
         self._client = Client(host=_DEFAULT_ENDPOINT)
 
     def get_response(self, prompt: str) -> str:
@@ -37,10 +44,24 @@ class LLMConnector:
         """Returns the headers for the request."""
         return {"Content-Type": "application/json"}
 
+    def _load_config(self, config_path: str) -> Dict[str, Any]:
+        """Loads the config from the given path.
+
+        Args:
+            config_path: Path to the config file.
+
+        Raises:
+            FileNotFoundError: If the file is not found.
+
+        Returns:
+            A dictionary containing the config keys and values.
+        """
+        if not os.path.isfile(config_path):
+            raise FileNotFoundError(f"File {config_path} not found.")
+        with open(config_path, "r") as file:
+            yaml_data = yaml.safe_load(file)
+        return yaml_data
+
     def _get_llm_config(self) -> Dict[str, Any]:
         """Returns the config for the request."""
-        return Options(
-            temperature=0.0,
-            top_p=0.9,
-            stop=["\n"],
-        )
+        return Options(self._config.get("options", {}))
